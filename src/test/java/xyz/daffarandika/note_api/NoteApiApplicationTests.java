c8411edb87aa44c33010db517303b8b3f443c0bb
Daffa Randika <daffa.randika.3@gmail.com>
2024-07-16 17:39:34 +0700

    Initial commit

---
+++ src/test/java/xyz/daffarandika/note_api/NoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.daffarandika.note_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
