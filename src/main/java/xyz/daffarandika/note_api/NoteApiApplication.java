9b3404932d20e75bb32629113e3828bac184184c
Daffa Randika <daffa.randika.3@gmail.com>
2024-07-17 09:24:41 +0700

    feat: basic OAuth 2.0 Resource Server JWT implementation

--- src/main/java/xyz/daffarandika/note_api/NoteApiApplication.java
+++ src/main/java/xyz/daffarandika/note_api/NoteApiApplication.java
@@ -2,7 +2,11 @@ package xyz.daffarandika.note_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import xyz.daffarandika.note_api.config.RsaKeyProperty;
+
+@EnableConfigurationProperties(RsaKeyProperty.class)
 @SpringBootApplication
 public class NoteApiApplication {
 
