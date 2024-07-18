b16d792b81deec4eabd9debe1194df9066354dd6
Daffa Randika <daffa.randika.3@gmail.com>
2024-07-18 17:43:04 +0700

    chore: added config for flyway and hibernate

--- src/main/java/xyz/daffarandika/note_api/NoteApiApplication.java
+++ src/main/java/xyz/daffarandika/note_api/NoteApiApplication.java
@@ -1,17 +1,31 @@
 package xyz.daffarandika.note_api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import xyz.daffarandika.note_api.config.RsaKeyProperty;
+import xyz.daffarandika.note_api.note.NoteRepository;
 
 @EnableConfigurationProperties(RsaKeyProperty.class)
 @SpringBootApplication
-public class NoteApiApplication {
+public class NoteApiApplication implements CommandLineRunner {
+
+	private final NoteRepository noteRepository;
+
+	public NoteApiApplication(NoteRepository noteRepository) {
+		this.noteRepository = noteRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		
 	}
 
 }
